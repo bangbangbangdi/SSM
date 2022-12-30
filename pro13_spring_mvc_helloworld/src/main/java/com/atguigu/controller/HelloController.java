92d3cff2f8980f274b1f8fbdc146d21135e6a56a
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-12-30 21:41:18 +0900

    xx

--- pro13_spring_mvc_helloworld/src/main/java/com/atguigu/controller/HelloController.java
+++ pro13_spring_mvc_helloworld/src/main/java/com/atguigu/controller/HelloController.java
@@ -1,7 +1,14 @@
 package com.atguigu.controller;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Component
+@Controller
 public class HelloController {
+
+    @RequestMapping("/")
+    public String protal(){
+        return "index";
+    }
+
 }
