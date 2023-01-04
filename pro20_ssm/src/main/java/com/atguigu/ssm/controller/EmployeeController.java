06943746d0c7536e7b1a1e7a7022af29c22f5596
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-01-05 01:50:18 +0900

    k

---
+++ pro20_ssm/src/main/java/com/atguigu/ssm/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.atguigu.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmployeeController {
+
+
+
+}
---
+++ pro20_ssm/src/main/java/com/atguigu/ssm/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.atguigu.ssm.service.impl;
+
+import com.atguigu.ssm.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+}
